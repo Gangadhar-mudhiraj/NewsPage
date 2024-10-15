63ba327980424930ae891dc0ac00833d408093dd
vishnuvardhnareddy <vr4507@gmail.com>
2024-10-15 20:36:40 +0530

    added all files

--- src/components/NewsBoard.jsx
+++ src/components/NewsBoard.jsx
@@ -5,23 +5,24 @@ export default function NewsBoard({ category }) {
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-    let URL = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=${
-      import.meta.env.VITE_API_KEY
-    }`;
-    fetch(URL).then((res) => 
+    let URL = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY
+      }`;
+    fetch(URL).then((res) =>
       res.json().then((data) => {
         setArticles(data.articles);
       })
     );
+
   }, [category]);
 
   return (
     <div>
-      <h2 className="text-center">
-        Latest <span className="badge bg-danger"> News</span>
+      <h2 className="text-center text-dark font-weight-bold">
+        Latest News<span className="badge bg-danger ms-4"> {category.toUpperCase()}</span>
+
       </h2>
       {articles.map((news, index) => {
-        return <NewsItem key={index} news={news} />;
+        return news && <NewsItem key={index} news={news} />;
       })}
     </div>
   );
